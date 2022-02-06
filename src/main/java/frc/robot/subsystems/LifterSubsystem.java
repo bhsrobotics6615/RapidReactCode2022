97c93be2d43e4628a906c15621ade8ee8d39fa8b
DragonMasterEv <99140065+DragonMasterEv@users.noreply.github.com>
2022-02-06 14:32:53 -0500

    Added the Lifter Subsystem

---
+++ src/main/java/frc/robot/subsystems/LifterSubsystem.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.robot.Variables;
+
+public class LauncherSubsystem extends SubsystemBase {
+  /** Creates a new LauncherSubsystem. */
+  
+  WPI_TalonSRX launcher = new WPI_TalonSRX(Constants.LAUNCHER);
+
+  public void rev() {
+    Variables.launcherSpeed = 1;
+    // Variables.launcherSpeed = Constants.speedConvConst / Math.sin(Variables.thetaDegrees);
+    launcher.set(Variables.launcherSpeed);
+  }
+
+  public void setLaunchSpeedAuto() {
+    double autoLaunchSpeed = Math.sqrt(Constants.GRAV * Math.pow(Variables.scoreDistance, 2) / (2 * (Math.pow(Math.cos(Constants.LAUNCH_ANGLE),2) * ((Variables.scoreDistance * Math.tan(Constants.LAUNCH_ANGLE)) - Constants.HIGH_SCORE))));
+    launcher.set(autoLaunchSpeed);
+  }
+
+  public void stop() {
+    launcher.set(0);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
