e96e3efe0ca76925b9d5cb1b814dcae0641c5b02
BHSRobotics6615 <bhsrobotics@vanburenschools.net>
2022-03-02 12:03:55 -0800

    test

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 //import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
@@ -10,22 +12,61 @@ public class DriveTrainSubsystem extends SubsystemBase {
 
     // public PWMTalonSRX frontLeft, backLeft, frontRight, backRight;
     public MecanumDrive drive;
+    private WPI_TalonSRX frontRight;
+    private WPI_TalonSRX backRight;
+    private WPI_TalonSRX backLeft;
+    private WPI_TalonSRX frontLeft;
 
     public DriveTrainSubsystem(){
 
         // Drive control motors
-        final WPI_TalonSRX frontRight = new WPI_TalonSRX(Constants.FRONT_RIGHT_MOTOR); // 2022 2
-        final WPI_TalonSRX backRight = new WPI_TalonSRX(Constants.BACK_RIGHT_MOTOR); // 2022 5
-        final WPI_TalonSRX backLeft = new WPI_TalonSRX(Constants.BACK_LEFT_MOTOR); // 2022 4
-        final WPI_TalonSRX frontLeft = new WPI_TalonSRX(Constants.FRONT_LEFT_MOTOR); // 2022 7
+        frontRight = new WPI_TalonSRX(Constants.FRONT_RIGHT_MOTOR); // 2022 2
+        backRight = new WPI_TalonSRX(Constants.BACK_RIGHT_MOTOR); // 2022 5
+        backLeft = new WPI_TalonSRX(Constants.BACK_LEFT_MOTOR); // 2022 4
+        frontLeft = new WPI_TalonSRX(Constants.FRONT_LEFT_MOTOR); // 2022 7
 
+       
 
+        
         //TODO: Invert the motors on either the left or the right side
         drive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
 
     }
 
    
+    public void encoderOn()
+    {
+        backLeft.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,0,10);
+        backRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,0,10);
+        frontLeft.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,0,10);
+        frontRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,0,10);
+
+        frontLeft.setSensorPhase(false);
+        frontRight.setSensorPhase(true);
+
+        frontLeft.setSelectedSensorPosition(0,0,10);
+        frontRight.setSelectedSensorPosition(0,0,10);
+
+        frontLeft.setInverted(true);
+        frontRight.setInverted(true);
+        backLeft.follow(frontLeft);
+        backRight.follow(frontRight);
+    }
+
+    public void encoderZero()
+    {
+        frontLeft.setSelectedSensorPosition(0,0,10);
+        frontRight.setSelectedSensorPosition(0,0,10);   
+    }
+    public double getDistance()
+    {
+    
+        double leftPosition = (frontLeft.getSelectedSensorPosition()/6900);
+        double rightPosition = (frontRight.getSelectedSensorPosition()/6900);
+        double distance = (leftPosition + rightPosition)/2;
+
+        return distance;
+    }
 
     
     public void moveAuto(double forwardSpeed, double horizontalSpeed, double rotationRate) { 
