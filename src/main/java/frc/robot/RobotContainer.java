389b4b3f37a382e8a6aa79b6e4df8f79c2c2adb0
BHSRobotics6615 <bhsrobotics@vanburenschools.net>
2022-03-08 16:45:27 -0500

    hehe

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,9 +108,9 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
+ /* public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     
     return run_auto;
-  }
+  }*/
 }
