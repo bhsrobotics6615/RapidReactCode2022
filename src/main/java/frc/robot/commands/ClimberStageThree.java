802353d2a5e9d46bfb243a3e8ab141ad676f7ea8
kalebcder <98268696+kalebcder@users.noreply.github.com>
2022-04-09 05:10:00 -0700

    Update ClimberStageThree.java

--- src/main/java/frc/robot/commands/ClimberStageThree.java
+++ src/main/java/frc/robot/commands/ClimberStageThree.java
@@ -4,16 +4,24 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Variables;
 import frc.robot.subsystems.ClimberSubsystem;
 
 public class ClimberStageThree extends CommandBase {
 
   private final ClimberSubsystem climbers;
-  public static boolean hasClimbEnded = false;
   public double startTime;
   
+  DigitalInput front_climb_right_limit_Switch = Variables.front_climb_right_limit_switch; 
+  DigitalInput front_climb_left_limit_Switch = Variables.front_climb_left_limit_switch; 
+  DigitalInput back_climb_limit_switch = Variables.back_climb_limit_switch;
+
+  
+
+
   /** Creates a new ClimberStageOne. */
   public ClimberStageThree(ClimberSubsystem climber_subsystem) {
     climbers = climber_subsystem;
@@ -29,23 +37,44 @@ public class ClimberStageThree extends CommandBase {
 
   @Override
   public void execute() {
-    while (Timer.getFPGATimestamp() < startTime + 2.3){
+    // while (Timer.getFPGATimestamp() < startTime + 2.3){
+      
+      climbers.front_right_climber_extend();
+      climbers.front_left_climber_extend();
+      double timepassed = startTime + 1.5;
+      if(front_climb_left_limit_Switch.get() == false && Timer.getFPGATimestamp() > timepassed ){
+          climbers.front_left_climber_stop();
+
+      }
+
+      if(front_climb_right_limit_Switch.get() == false &&  Timer.getFPGATimestamp() > timepassed ){
+        climbers.front_right_climber_stop();
+
+    }
+              /*while (Timer.getFPGATimestamp() < startTime + 2.3){
       climbers.front_climber_extend();
     }
     if (Timer.getFPGATimestamp() >= startTime + 2.3){
       hasClimbEnded = true;
-    }
+    }*/
+  
+    // }
+    // if (Timer.getFPGATimestamp() >= startTime + 2.3){
+      // Variables.hasClimbEnded = true;
+    // }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     climbers.front_climber_stop();
+    Variables.hasClimbEnded = false;
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return hasClimbEnded;
+    return Variables.hasClimbEnded;
   }
 }
+   
